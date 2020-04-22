7c3e1791bbef5962e12f5bd561cf42cadcfa2ee0
Kervin <dudu3321@hotmail.com>
2020-04-22 17:56:29 +0800

    info component finished

--- ClientApp/src/components/MainForm/Info/Info.jsx
+++ ClientApp/src/components/MainForm/Info/Info.jsx
@@ -1,28 +1,48 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
 import './Info.styles.scss';
 
-export default class Info extends PureComponent {
+class Info extends PureComponent {
   constructor(props) {
     super(props)
-    const {LatePending} = this.props;
-    this.state = {
-      Added: 0,
-      Stocks: 0,
-      TradeIns: 0,
-      LatePending: LatePending,
-      UpdateTime: ''
-    }
+  }
+
+  componentDidMount = () => {
+    this.intervalId = setInterval(() => {this.forceUpdate();}, 10000);
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.intervalId);
   }
 
   render() {
+    const { resultDetailData } = this.props;
     return (
       <div>
-        <label>Added: {this.state.Added}</label>
-        <label>Stocks: {this.state.Stocks}</label>
-        <label>Trade Ins: {this.state.TradeIns}</label>
-        <label>Late pending: {this.state.LatePending}</label>
-        <label>{this.state.Stocks}</label>
+        <label>Added: {resultDetailData.added ?? 0}</label>
+        <label>Stocks: {resultDetailData.stocks ?? 0}</label>
+        <label>Trade Ins: {resultDetailData.tradeIns ?? 0}</label>
+        <label>Late pending: {resultDetailData.latePending ?? 0}</label>
       </div>
     )
   }
 }
+
+
+const mapStateToProps = (state, props) => {
+  const { tabIndex } = props;
+  let resultData = Object.assign({}, state.resultData);
+  if (Object.keys(resultData).length > 0) {
+    if (typeof resultData.resultDetailData[tabIndex] !== 'undefined') {
+      resultData = resultData.resultDetailData[tabIndex];
+    }
+  }
+  return {
+    resultDetailData: resultData
+  }
+};
+const mapDispatchToProps = () => { 
+  return {}
+};
+export default connect(mapStateToProps,
+  mapDispatchToProps)(Info) 
